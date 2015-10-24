f056dacc179f158b50db44555668bd0fe9e0be1c
benjaminran <benjaminran2@gmail.com>
2015-10-24 17:31:28 -0400

    auto

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -31,8 +31,8 @@ def connect(start_term):
 def commit_urls(urls):
     global cur
     for url in urls:
-        print('Executing SQL: '+'INSERT INTO sites (url, visits) SELECT \'%s\', 1000 FROM DUAL WHERE NOT EXISTS (SELECT url FROM sites WHERE url=\'%s\') LIMIT 1;' % (url, url))
-        cur.execute('INSERT INTO sites (url, visits) SELECT \'%s\', 1000 FROM DUAL WHERE NOT EXISTS (SELECT url FROM sites WHERE url=\'%s\') LIMIT 1;', (url, url))
+        print(url)
+        cur.execute('INSERT INTO sites (url, visits) SELECT %s, 1000 FROM DUAL WHERE NOT EXISTS (SELECT url FROM sites WHERE url=%s) LIMIT 1;', (url, url))
 
 def get_alchemy_concepts(url):
     """ TODO """
