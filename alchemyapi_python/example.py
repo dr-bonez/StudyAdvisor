c2641cf5897ba54dd0aea01108e821268646488d
Aiden McClelland <gagglehoof@gmail.com>
2015-10-24 11:07:02 +0000

    fixed sql query

--- alchemyapi_python/example.py
+++ alchemyapi_python/example.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from alchemyapi import AlchemyAPI
-import json, mysql.connector
+import json, mysql.connector, math
 
 GAIN = 2.0
+weight = 1000.0
 alchemyapi = AlchemyAPI()
 
 def connect():
@@ -12,6 +13,7 @@ def connect():
 		if not (conn.is_connected()):
 			print('Could not connect to MySQL database')
 			exit()
+		main(conn)
 	except mysql.connector.Error as e:
 		print(e)
 		exit()
@@ -32,13 +34,27 @@ def compareconcepts(urla, urlb):
 		total += float(concepta['relevance'])
 		for conceptb in responseb['concepts']:
 			if conceptb['text'] == concepta['text']:
-				connect += float(conceptb['relevance']) * float(concepta['relevance'])
-	print(total)
+				connect += math.sqrt(float(conceptb['relevance']) * float(concepta['relevance']))
 	return GAIN * connect / total
+
+def main(conn):
+	cursora = conn.cursor(buffered=True)
+	cursorb = conn.cursor(buffered=True)
+	cursori = conn.cursor(buffered=True)
+	cursora.execute("SELECT id, url FROM sites")
+	rowa = cursora.fetchone()
+	while rowa is not None:
+		cursorb.execute("SELECT id, url FROM sites")
+		rowb = cursorb.fetchone()
+		while rowb is not None:
+			if (rowa[0] != rowb[0]):
+				cursori.execute("INSERT INTO connections (site_id, from_id, connections) SELECT " + str(rowa[0]) + ", " + str(rowb[0]) + ", " + str(compareconcepts(str(rowa[1]), str(rowb[1])) * weight) + " FROM dual WHERE NOT EXISTS (SELECT 1 FROM connections WHERE (site_id=" + str(rowa[0]) + " AND from_id=" + str(rowb[0]) + ") OR (site_id=" + str(rowb[0]) + " AND from_id=" + str(rowa[0]) + ")")
+			rowb = cursorb.fetchone()
+		rowa = cursora.fetchone()
+	conn.commit()
 
 if __name__ == '__main__':
 	connect()
-	print(compareconcepts("https://en.wikipedia.org/wiki/Mathematics", "http://mathematics.stanford.edu/"))
 #response = alchemyapi.concepts('url', demo_url)
 
 #if response['status'] == 'OK':
