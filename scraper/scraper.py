345868f22eb81c1eb791a735d25fc384fea21a28
benjaminran <benjaminran2@gmail.com>
2015-10-24 08:35:23 -0400

    fixed indentation

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -12,18 +12,18 @@ concepts_interned = []
 def commit_url(url):
     """ Add all urls from urls_interned list to mySQL database """
     try:
-                conn = mysql.connector.connect(host='localhost', database='study', user='root', password='password')
-                if not (conn.is_connected()):
-                        print('Could not connect to MySQL database')
-                        exit()
-                cur = conn.cursor()
+        conn = mysql.connector.connect(host='localhost', database='study', user='root', password='password')
+        if not (conn.is_connected()):
+            print('Could not connect to MySQL database')
+            exit()
+        cur = conn.cursor()
         cur.execute('INSERT INTO sites (url, visits) SELECT url FROM (SELECT '+url+') AS tmp WHERE NOT EXISTS (SELECT url FROM sites WHERE url='+url+') LIMIT 1;')
         conn.commit()
-        except mysql.connector.Error as e:
-                print(e)
-                exit()
-        else:
-                conn.close()
+    except mysql.connector.Error as e:
+        print(e)
+        exit()
+    else:
+        conn.close()
 
 def get_alchemy_concepts(url):
     """ TODO """
