21e399f4c6115ff4e2778a922433ada3773fff4d
Aiden McClelland <akmcclel@ucsc.edu>
2015-10-24 23:53:44 +0000

    fixed erryting

--- util/update-api-key.sh
+++ util/update-api-key.sh
@@ -3,7 +3,7 @@
 newkey="$1"
 
 rm alchemyapi_python/api_key.txt
-python alchemyapi_python/alchemyapi.py \"${newkey}\"
+python alchemyapi_python/alchemyapi.py "${newkey}"
 
-rm scaper/api_key.txt
-python scraper/alchemyapi.py \"${newkey}\"
+rm scraper/api_key.txt
+python scraper/alchemyapi.py "${newkey}"
