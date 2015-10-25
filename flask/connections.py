3a160f30477a21e80ba35d9896b65032aee32eb2
benjaminran <benjaminran2@gmail.com>
2015-10-25 00:18:34 -0400

    auto

--- flask/connections.py
+++ flask/connections.py
@@ -21,7 +21,9 @@ conn = mysql.connector.connect(host='localhost', database='study', user='root', 
 def get_recent_sites(uid, n=1):
     """ return a list of the last n siteids a user has visited """
     cur = conn.cursor()
-    return cur.execute('SELECT site_id FROM users_join ORDER BY date DESC LIMIT 5;').fetchall()
+    recents = cur.execute('SELECT site_id FROM users_join ORDER BY date DESC LIMIT 5;').fetchall()
+    conn.commit()
+    return recents;
 
 def get_weight(site):
     cur = conn.cursor()
