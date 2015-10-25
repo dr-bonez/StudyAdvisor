5edecf930ead4f8971bac604f9e8de1971fc3c4a
CameronOC <cjplanes@gmail.com>
2015-10-25 01:50:52 -0700

    changed the ui

--- proj/popup.js
+++ proj/popup.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var btn1 = document.getElementById('btn1');
  // onClick's logic below:
     btn1.addEventListener('click', function() {
-      
+      chrome.tabs.executeScript({
+			code: 'document.body.style.backgroundColor="red"'
+		});
     });
 });
 
