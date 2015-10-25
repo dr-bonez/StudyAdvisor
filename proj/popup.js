1bcce7e7df3dc14a1ef57a8b35786ba62520fdd6
trupasin <trupasin@The.Scarred.Legend>
2015-10-24 17:53:53 -0700

    updated front end

--- proj/popup.js
+++ proj/popup.js
@@ -1,18 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     var btn1 = document.getElementById('btn1');
-    // onClick's logic below:
+ var btn2 = document.getElementById('btn2');   
+ // onClick's logic below:
     btn1.addEventListener('click', function() {
       alert("u fuckin wot m8");
     });
-});
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var btn2 = document.getElementById('btn2');
-    // onClick's logic below:
-    btn2.addEventListener('click', function() {
+btn2.addEventListener('click', function() {
       alert("What the fuck did you just say to me  you little bitch");
     });
+
 });
+
 
